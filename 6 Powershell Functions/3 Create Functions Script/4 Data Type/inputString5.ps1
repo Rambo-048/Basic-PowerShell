c85d868ebd651afefed85fc1ca829a75ef7b8eff
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2025-12-27 23:40:18 +0700

    Add files via upload

---
+++ 6 Powershell Functions/3 Create Functions Script/4 Data Type/inputString5.ps1
@@ -0,0 +1,10 @@
+Function Input-String([string]$dataString)
+{
+"Do Not Let Yesterday Take Up Too Much Of Today"
+}#End Input-String 
+
+# *** Entry Point To Script ***
+
+#Input-String
+
+Input-String "Input Data String"
---
+++ 6 Powershell Functions/3 Create Functions Script/4 Data Type/inputString6.ps1
@@ -0,0 +1,10 @@
+Function Input-String([string]$dataString)
+{
+"Not Many People Will Accompany You When They See You In A Situation That Could Harm Them"
+$dataString
+}#End Input-String 
+
+# *** Entry Point To Script ***
+
+Input-String
+Input-String -dataString "Input Data String"
