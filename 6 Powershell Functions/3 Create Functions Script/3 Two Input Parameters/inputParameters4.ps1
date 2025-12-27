0ae7f707aa23793feecedc70a8ee5a5e68159bb6
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2025-12-27 23:39:57 +0700

    Add files via upload

---
+++ 6 Powershell Functions/3 Create Functions Script/3 Two Input Parameters/inputParameters4.ps1
@@ -0,0 +1,16 @@
+Function Input-Parameter($data_A, $data_B, $data_C, $data_D, $data_E)
+{
+$data_A = "It Is Like Life Together But You Still Feeling Lonely"
+$data_B = "Sometimes Feeling Doubts Keep You From Moving"
+$data_C = "Forced To Be Strong, Facing So Much Pressure"
+$data_D = "Treat People Well, Even If You Are Genius"
+$data_E = "Not Many People Will Accompany You When They See You In A Situation That Could Harm Them"
+
+$data_A
+$data_B
+$data_C
+$data_D
+$data_E
+} #end Input-Parameter
+
+Input-Parameter
