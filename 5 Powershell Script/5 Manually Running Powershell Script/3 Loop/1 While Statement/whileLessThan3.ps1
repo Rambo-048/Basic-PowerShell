e0ece9294dea9d667f7de8e4ecc7d3be20995daa
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2025-12-27 22:02:20 +0700

    Add files via upload

---
+++ 5 Powershell Script/5 Manually Running Powershell Script/3 Loop/1 While Statement/whileLessThan3.ps1
@@ -0,0 +1,6 @@
+$i = 0
+While ($i -lt 15)
+{
+"`$i --> $i. less than 15"
+$i++
+} #end while $i lt 15
---
+++ 5 Powershell Script/5 Manually Running Powershell Script/3 Loop/1 While Statement/whileLessThan4.ps1
@@ -0,0 +1,6 @@
+$i = 0
+While ($i -lt 20)
+{
+"`$i --> $i. less than 20"
+$i++
+} #end while $i lt 20
