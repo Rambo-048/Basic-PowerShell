00deb4918e9f58cdfa18c883a98266def23600b8
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2025-12-27 23:37:20 +0700

    Add files via upload

---
+++ 6 Powershell Functions/3 Create Functions Script/1 Simple Script Function/convertData3.ps1
@@ -0,0 +1,9 @@
+Function convertData($a)
+{
+$a = 20
+"0.100 dam => 0.100 x $a --> $(.100 * 10) meter"
+} #end convertData
+
+convertData
+# convertData
+# convertData -feet $a
---
+++ 6 Powershell Functions/3 Create Functions Script/1 Simple Script Function/convertData4.ps1
@@ -0,0 +1,9 @@
+Function convertData($a)
+{
+$a = 20
+"100 cm => 100 / $a --> $(100 / $a) meter"
+} #end convertData
+
+convertData
+# convertData
+# convertData -feet $a
